5baa870746d8cb0c8b796431205903261e3a8f33
James Hibbard <james.hibbard@sitepoint.com>
2017-10-16 20:48:29 +0200

    Add search filter functionality

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -2,33 +2,73 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const EventList = ({ events, activeId }) => (
-  <section>
-    <div className='events-container'>
-      <h2>
-        Events
-        <Link to='/events/new'>New Event</Link>
-      </h2>
+class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
 
-      <input className='search' placeholder='Search' type='text' />
+    this.filterEvents = this.filterEvents.bind(this);
+  }
 
-      <ul className='events-list'>
-        {
-          events.map(event => (
-            <li
-              key={event.id}
-              className={(activeId === event.id) ? 'active' : ''}
-            >
-              <Link to={`/events/${event.id}`}>
-                {event.event_date} - {event.event_type}
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  </section>
-);
+  filterEvents() {
+    this.setState({ searchTerm: this.searchInput.value });
+  }
+
+  match(obj) {
+    const {
+      id,
+      host_multiple,
+      speaker_multiple,
+      published,
+      created_at,
+      updated_at,
+      ...rest } = obj;
+
+    return Object.values(rest).some(value => (
+      value.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) > -1
+    ));
+  }
+
+  render() {
+    const events = this.props.events.filter(el => this.match(el));
+
+    return (
+      <section>
+        <div className='events-container'>
+          <h2>
+            Events
+            <Link to='/events/new'>New Event</Link>
+          </h2>
+
+          <input
+            className='search'
+            placeholder='Search'
+            type='text'
+            ref={(input) => { this.searchInput = input; }}
+            onKeyUp={this.filterEvents}
+          />
+
+          <ul className='events-list'>
+            {
+              events.map(event => (
+                <li
+                  key={event.id}
+                  className={(this.props.activeId === event.id) ? 'active' : ''}
+                >
+                  <Link to={`/events/${event.id}`}>
+                    {event.event_date} - {event.event_type}
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
 
 EventList.propTypes = {
   activeId: PropTypes.number,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'site#index'
   get 'login', to: 'site#index'
-  get 'new', to: 'site#index'
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
   get 'events/:id', to: 'site#index'
   get 'events/:id/edit', to: 'site#index'
 
